224f3cf7bcf96c1bedef3fa7664ef89ef819b925
dennis doe <dennisdoesports@gmail.com>
2026-03-27 15:29:16 +0100

    refactor(home): comment out AboutRefreshSection component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <AboutRefreshSection />
+      {/* <AboutRefreshSection /> */}
       <CommunitySection />
       <TestimonialSection />
       <Footer />
